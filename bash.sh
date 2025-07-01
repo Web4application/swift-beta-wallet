ab55320a1528063fce9acc0d784db020de1d47b2
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-01 04:04:38 +0200

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,3 @@
+git config --global user.name "SwiftLogger"
+git config --global user.email "log@fadaka.ai"
+git remote set-url origin https://<your-github-token>@github.com/Web4application/Fadaka-blockchain.git
